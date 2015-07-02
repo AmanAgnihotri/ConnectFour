01926092370f151edde327619a2c0d04f02925d8
shkesar <shubhamkesar@live.com>
2015-07-03 01:08:48 +0530

    Code formatting
    Re-formatted code to improve reading.

--- ConnectFour.scala
+++ ConnectFour.scala
@@ -41,16 +41,16 @@ class ConnectFour {
     }
   }
 
-  def isGridFull: Boolean = fillerIndex.filter(_ < 0).size equals COLUMNS
+  def isGridFull: Boolean = fillerIndex.filter(_ < 0).size == COLUMNS
 
   def checkGrid(tile: Tile): Boolean = {
     var (i, j, k, m, counter) = (0, 0, 0, 0, 0)
 
     // Check downward
     for (
-      i <- 0 to ROWS - CONSECUTIVE_CONNECTION_REQUIRED;
-      j <- 0 until COLUMNS
-    ) {
+    i <- 0 to ROWS - CONSECUTIVE_CONNECTION_REQUIRED;
+    j <- 0 until COLUMNS)
+    {
       if ((i until CONSECUTIVE_CONNECTION_REQUIRED + i).count(
         grid(_)(j) equals tile
       ) == CONSECUTIVE_CONNECTION_REQUIRED)
@@ -59,9 +59,9 @@ class ConnectFour {
 
     // Check across
     for (
-      i <- 0 to COLUMNS - CONSECUTIVE_CONNECTION_REQUIRED;
-      j <- 0 until ROWS
-    ) {
+    i <- 0 to COLUMNS - CONSECUTIVE_CONNECTION_REQUIRED;
+    j <- 0 until ROWS)
+    {
       if ((i until CONSECUTIVE_CONNECTION_REQUIRED + i).count(
         grid(j)(_) equals tile
       ) == CONSECUTIVE_CONNECTION_REQUIRED)
@@ -70,9 +70,9 @@ class ConnectFour {
 
     // Check left to right diagonally
     for (
-      i <- 0 to ROWS - CONSECUTIVE_CONNECTION_REQUIRED;
-      j <- 0 to COLUMNS - CONSECUTIVE_CONNECTION_REQUIRED
-    ) {
+    i <- 0 to ROWS - CONSECUTIVE_CONNECTION_REQUIRED;
+    j <- 0 to COLUMNS - CONSECUTIVE_CONNECTION_REQUIRED)
+    {
       if ((i until CONSECUTIVE_CONNECTION_REQUIRED + i)
         .zip(j until CONSECUTIVE_CONNECTION_REQUIRED + j)
         .count { case (k, m) =>
@@ -83,9 +83,9 @@ class ConnectFour {
 
     // Check right to left diagonally
     for (
-      i <- 0 to ROWS - CONSECUTIVE_CONNECTION_REQUIRED;
-      j <- (COLUMNS - 1) until (COLUMNS - CONSECUTIVE_CONNECTION_REQUIRED) by -1
-    ) {
+    i <- 0 to ROWS - CONSECUTIVE_CONNECTION_REQUIRED;
+    j <- (COLUMNS - 1) until (COLUMNS - CONSECUTIVE_CONNECTION_REQUIRED) by -1)
+    {
       if ((i until CONSECUTIVE_CONNECTION_REQUIRED + i)
         .zip(j until j - CONSECUTIVE_CONNECTION_REQUIRED by -1)
         .count { case (k, m) =>
@@ -105,14 +105,16 @@ class ConnectFour {
 
     var position = 0
     do (position = scala.io.StdIn.readInt) while (!isValid)
-
-    def isValid: Boolean = position >= 0 && position < COLUMNS && fillerIndex(position) >= 0
+    def isValid: Boolean = position >= 0 && position < COLUMNS &&
+      fillerIndex(position) >= 0
 
     grid(fillerIndex(position))(position) = tile
     fillerIndex(position) -= 1
   }
 
-  def gridToString: String = grid.map("|" + _.map(_.color).mkString("|") + "|").mkString("\n")
+  def printGrid: String = println(grid.map(
+    "|" + _.map(_.color).mkString("|") + "|"
+  ).mkString("\n"))
 
 }
 
